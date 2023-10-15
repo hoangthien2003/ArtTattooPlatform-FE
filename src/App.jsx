937e87a6d44b3feab235d0b59eb6324619f8e7ae
hoangthien2003 <hoangdangbaothien@gmail.com>
2023-10-16 04:32:27 +0700

    Add navbar v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { useState } from "react";
 import theme from "./lib/Theme";
-import Home from "./pages/Home";
 import Footer from "./components/Footer/Footer";
-import { CssBaseline } from "@mui/material";
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   const HomePage = lazy(() => import("./pages/Home"));
 
   return (
     <BrowserRouter basename="/">
-     <ThemeProvider theme={theme}>
-       <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
         <Suspense fallback={<p>Loading...</p>}>
           <Routes>
             <Route path="/" element={<HomePage />} />
           </Routes>
           <Footer />
         </Suspense>
-     </ThemeProvider>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
