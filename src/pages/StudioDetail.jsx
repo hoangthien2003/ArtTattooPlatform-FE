bfcda1724c26c645ef830dda851e604bfdd30f4d
Dan <brunotran1602@gmail.com>
2023-10-27 00:36:08 +0700

    studioDetail update

--- src/pages/StudioDetail.jsx
+++ src/pages/StudioDetail.jsx
@@ -1,70 +1,182 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Container, Stack } from "@mui/system";
+import { Link, useParams } from "react-router-dom";
+import { Breadcrumbs, Button, CardActionArea, CardActions, CardContent, CardMedia, Container, Grid, Modal, Stack } from "@mui/material";
 import { Avatar, Card, Rating, Typography } from "@mui/material";
+import Booking from "../components/Modal/Booking";
+import CardService from "../components/Card/CardService";
+import Slider from "react-slick";
+import { Home } from "@mui/icons-material";
+import { StarList } from "../components/Feedback/StarList";
+import FeedbackForm from "../components/Feedback/FeedbackForm";
+import CommentList from "../components/Feedback/CommentList";
 
 export default function StudioDetail() {
   const { studioId } = useParams();
 
-  const [data, setData] = useState();
+  const [studioData, setStudioData] = useState(null);
+  const [serviceData, setServiceData] = useState(null);
 
   useEffect(() => {
     getStudioID();
+    getServiceByStudioId();
   }, []);
 
   const getStudioID = async () => {
-    await axios
-      .get(
+    try {
+      const response = await axios.get(
         import.meta.env.VITE_REACT_APP_API_URL +
         `/Studio/GetStudioByID/${studioId}`
-      )
-      .then((res) => {
-        // setData(res.data);
-        setData(res.data);
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      );
+      setStudioData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
+  const getServiceByStudioId = async () => {
+    try {
+      const response = await axios.get(
+        import.meta.env.VITE_REACT_APP_API_URL +
+        `/Service/GetServiceByStudio/${studioId}`
+      );
+      setServiceData(response.data.$values);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const settings = {
+    // className: "center",
+    dots: false,
+    infinite: true,
+    centerPadding: "50px",
+    speed: 500,
+    slidesToShow: 3,
+    swipeToSlide: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+  const ratings = [
+    { rate: 5, count: 100 },
+    { rate: 4, count: 50 },
+    { rate: 3, count: 20 },
+    { rate: 2, count: 10 },
+    { rate: 1, count: 5 },
+  ];
   return (
-    <Container>
+    <Container className='mt-5 mb-5'>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Stack
+          spacing={1}
+          direction={"row"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Home fontSize="inherit" />
+          <Typography
+            variant="body1"
+            component={Link}
+            to="/"
+            sx={{ textDecoration: "none" }}
+          >
+            Home
+          </Typography>
+        </Stack>
+        <Typography
+          variant="body1"
+          component={Link}
+          to="/services"
+          sx={{ textDecoration: "none" }}
+        >
+          Service
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{ textDecoration: "none" }}
+        >
+          {studioData && studioData.studioName}
+        </Typography>
+      </Breadcrumbs>
       <div className="row mt-5 mb-5">
         <div className="col-md-4">
-          <Card >
-            <Stack direction="column" justifyContent={'center'} alignItems={'center'} >
-              <Avatar sx={{ width: 100, height: 100, borderColor:'white' }} className='mt-5 mb-4' src={data && data.logo} />
-              <Typography gutterBottom variant="h5" component="div" sx={{ textAlign: 'center' }}  className='mb-1'>
-                {data && data.studioName}
+          <Card>
+            <Stack direction="column" justifyContent={'center'} alignItems={'center'}>
+              <Avatar sx={{ width: 100, height: 100, borderColor: 'white' }} className='mt-5 mb-4' src={studioData && studioData.logo} />
+              <Typography gutterBottom variant="h5" component="div" sx={{ textAlign: 'center' }} className='mb-1'>
+                {studioData && studioData.studioName}
               </Typography>
-              <Typography gutterBottom variant="h6" component="div" sx={{ textAlign: 'center' }}  className='mb-1'>
-                {data && data.description}
+              <Typography gutterBottom variant="h6" component="div" sx={{ textAlign: 'center' }} className='mb-1'>
+                {studioData && studioData.description}
               </Typography>
               <Rating name="size-small" defaultValue={4} size="small" />
-              <Typography gutterBottom variant="h7" component="div" sx={{ textAlign: 'center' }}  className='mb-1'>
-              {data && data.studioEmail}
+              <Typography gutterBottom variant="h7" component="div" sx={{ textAlign: 'center' }} className='mb-1'>
+                {studioData && studioData.studioEmail}
               </Typography>
-              <Typography gutterBottom variant="h7" component="div" sx={{ textAlign: 'center' }}  className='mb-1'>
-              {data && data.studioPhone}
+              <Typography gutterBottom variant="h7" component="div" sx={{ textAlign: 'center' }} className='mb-1'>
+                {studioData && studioData.studioPhone}
               </Typography>
               <Typography gutterBottom variant="h7" component="div" sx={{ textAlign: 'center' }} className='mb-5'>
-              {data && data.address}
+                {studioData && studioData.address}
               </Typography>
             </Stack>
           </Card>
-
+          <Typography variant="h5" className="mt-3">Our ratings</Typography>
+          {/* give reviews and write review content */}
+                <StarList  ratings={ratings}  />
+        
         </div>
-        <div className="col-md-8" style={{ backgroundColor: 'white' }}>
-          aaaaaaaaaaaaaaaa
+        <div className="col-md-8">
+          <Container>
+            <Typography gutterBottom variant="h6" component="div" sx={{ textAlign: 'center' }} className="mb-4">
+              OUR SERVICE
+            </Typography>
+            <Slider {...settings} className="ps-4">
+              {serviceData && serviceData.map((dataService, index) => {
+                return (
+                  <CardService key={index}
+                    serviceId={dataService.serviceId}
+                    serviceName={dataService.serviceName}
+                    studioId={dataService.studioId}
+                    rate={dataService.rating}
+                    description={dataService.description}
+                    imageService={dataService.imageService}
+                    price={dataService.price}
+                  />
+                );
+              })}
+            </Slider>
+          </Container>
         </div>
       </div>
-      {/* <Typography gutterBottom variant="h5" component="div">
-          {data && data.address}
-        </Typography> */}
     </Container>
   );
 }
-
