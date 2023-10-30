7997263eba1857d6a7446e65977c7f258f899a43
hoangthien2003 <hoangdangbaothien@gmail.com>
2023-10-30 13:48:51 +0700

    Add: login authorize

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./lib/Theme";
 import Footer from "./components/Footer/Footer";
-import React, { Suspense, lazy } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { Suspense, lazy, useEffect, useState } from "react";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { useGoogleOneTapLogin } from "@react-oauth/google";
 // import HomePage from "./pages/Home";
 // import Service from "./pages/Service";
@@ -13,44 +13,69 @@ import { ToastContainer } from "react-toastify";
 import ProfilePage from "./pages/ProfilePage";
 import Service from "./pages/Service";
 import DashboardManagerPage from "./pages/BookingManagement";
-import BookingManagement from "./pages/BookingManagement";
+import { useUserInfo } from "./stores/useUserInfo";
+import jwtDecode from "jwt-decode";
 
 function App() {
-  const HomePage = lazy(() => import("./pages/Home"));
-  const ServiceDetail = lazy(() => import("./pages/ServiceDetail"));
-  const Service = lazy(() => import("./pages/Service"));
-  const Navbar = lazy(() => import("./components/Navbar/Navbar"));
-  useGoogleOneTapLogin({
-    onSuccess: (credentialResponse) => {
-      console.log(credentialResponse);
-    },
-    onError: () => {
-      console.log("Login failed!");
-    },
-  });
+	const HomePage = lazy(() => import("./pages/Home"));
+	const ServiceDetail = lazy(() => import("./pages/ServiceDetail"));
+	const Service = lazy(() => import("./pages/Service"));
+	const Navbar = lazy(() => import("./components/Navbar/Navbar"));
+	const NotAccess = lazy(() => import("./pages/NotAccess"));
+	const BookingManagement = lazy(() => import("./pages/BookingManagement"));
+	const NotFound = lazy(() => import("./pages/NotFound"));
 
-  return (
-    <BrowserRouter basename="/">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <ToastContainer />
-        <Suspense fallback={<p>Loading...</p>}>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/services" element={<Service />} />
-            <Route path="/services/:serviceId" element={<ServiceDetail />} />
-            <Route path="/StudioPage" element={<StudioPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/BookingManagement" element={<BookingManagement />} />
-            <Route path="/StudioDetail/:studioId" element={<StudioDetail />}
-            ></Route>
-          </Routes>
-          <Footer />
-        </Suspense>
-      </ThemeProvider>
-    </BrowserRouter>
-  );
+	const [role, setRole] = useState();
+
+	useEffect(() => {
+		const token = localStorage.getItem("token");
+		if (token != null) {
+			const user = jwtDecode(token);
+			setRole(user.role);
+		}
+	}, []);
+
+	useGoogleOneTapLogin({
+		onSuccess: (credentialResponse) => {
+			console.log(credentialResponse);
+		},
+		onError: () => {
+			console.log("Login failed!");
+		},
+	});
+
+	return (
+		<BrowserRouter basename="/">
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<ToastContainer />
+				<Suspense fallback={<p>Loading...</p>}>
+					<Navbar />
+					<Routes>
+						<Route path="*" exact={true} element={<NotFound />} />
+						<Route path="/" element={<HomePage />} />
+						<Route path="/services" element={<Service />} />
+						<Route
+							path="/services/:serviceId"
+							element={<ServiceDetail />}
+						/>
+						<Route path="/StudioPage" element={<StudioPage />} />
+						<Route path="/profile" element={<ProfilePage />} />
+						<Route
+							path="/BookingManagement"
+							element={<BookingManagement role={role} />}
+						/>
+						<Route
+							path="/StudioDetail/:studioId"
+							element={<StudioDetail />}
+						></Route>
+						<Route path="/access-denied" element={<NotAccess />} />
+					</Routes>
+					<Footer />
+				</Suspense>
+			</ThemeProvider>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,42 +4,54 @@ import SliderCard from "../components/Slider/SliderCard";
 import axios from "axios";
 import PaginationCard from "../components/Pagination/PaginationCard";
 import TopRateStudio from "../components/TopRate/TopRateStudio";
+import jwtDecode from "jwt-decode";
+import { useUserInfo } from "../stores/useUserInfo";
 const Home = () => {
-  const [serviceList, setServiceList] = useState([]);
+	const [serviceList, setServiceList] = useState([]);
+	const setUser = useUserInfo((state) => state.setUser);
+	useEffect(() => {
+		const token = localStorage.getItem("token");
+		if (token) {
+			const user = jwtDecode(token);
+			setUser(user);
+		}
+		getServiceList();
+	}, []);
 
-  useEffect(() => {
-    getServiceList();
-  }, []);
+	const getServiceList = async () => {
+		await axios
+			.get(import.meta.env.VITE_REACT_APP_API_URL + "/Service/GetAll")
+			.then((res) => {
+				// console.log(res.data.$values);
+				setServiceList(res.data.$values);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-  const getServiceList = async () => {
-    await axios
-      .get(import.meta.env.VITE_REACT_APP_API_URL + "/Service/GetAll")
-      .then((res) => {
-        // console.log(res.data.$values);
-        setServiceList(res.data.$values);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  return (
-    <Container maxWidth="lg" sx={{ marginTop: 5, marginBottom: 5 }}>
-      <Stack spacing={3} direction={"column"} paddingTop={5} paddingBottom={5}>
-        <Typography variant="h5">TOP RATE SERVICE</Typography>
-        <SliderCard serviceList={serviceList} />
-        <Typography variant="h5">NEW SERVICE</Typography>
-      </Stack>
-      <Grid container spacing={6} columns={{ xs: 4, sm: 8, md: 12 }}>
-        <Grid item xs={8}>
-          <PaginationCard serviceList={serviceList} />
-        </Grid>
-        <Grid item xs={4}>
-          <TopRateStudio />
-        </Grid>
-      </Grid>
-    </Container>
-  );
+	return (
+		<Container maxWidth="lg" sx={{ marginTop: 5, marginBottom: 5 }}>
+			<Stack
+				spacing={3}
+				direction={"column"}
+				paddingTop={5}
+				paddingBottom={5}
+			>
+				<Typography variant="h5">TOP RATE SERVICE</Typography>
+				<SliderCard serviceList={serviceList} />
+				<Typography variant="h5">NEW SERVICE</Typography>
+			</Stack>
+			<Grid container spacing={6} columns={{ xs: 4, sm: 8, md: 12 }}>
+				<Grid item xs={8}>
+					<PaginationCard serviceList={serviceList} />
+				</Grid>
+				<Grid item xs={4}>
+					<TopRateStudio />
+				</Grid>
+			</Grid>
+		</Container>
+	);
 };
 
 export default Home;
