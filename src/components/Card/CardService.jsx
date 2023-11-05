4c854b8ea7ab70a49a12b47a482db1cf925b26da
hoangthien2003 <hoangdangbaothien@gmail.com>
2023-11-05 19:23:47 +0700

    done: booking

--- src/components/Card/CardService.jsx
+++ src/components/Card/CardService.jsx
@@ -1,15 +1,16 @@
 import {
-  Avatar,
-  Button,
-  Card,
-  CardActionArea,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Modal,
-  Rating,
-  Stack,
-  Typography,
+	Avatar,
+	Button,
+	Card,
+	CardActionArea,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Modal,
+	Rating,
+	Skeleton,
+	Stack,
+	Typography,
 } from "@mui/material";
 import axios from "axios";
 import React, { forwardRef, useEffect, useState } from "react";
@@ -18,126 +19,129 @@ import "../../styles/CardService.css";
 import Booking from "../Modal/Booking";
 
 const CardService = (props) => {
-  const { serviceId, serviceName, studioId, price, imageService, rate } = props;
-  const [open, setOpen] = useState(false);
-  const [studioName, setStudioName] = useState("");
-  const [studioLogo, setStudioLogo] = useState("");
-  // console.log("rate: ", rate);
-  // console.log("props: ", props);
-  const [data, setData] = useState();
+	const { serviceId, serviceName, studioId, price, imageService, rate } =
+		props;
+	const [open, setOpen] = useState(false);
+	const [studioName, setStudioName] = useState("");
+	const [studioLogo, setStudioLogo] = useState("");
+	// console.log("rate: ", rate);
+	// console.log("props: ", props);
+	const [data, setData] = useState();
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    getStudio();
-    getServiceById();
-  }, []);
+	useEffect(() => {
+		getStudio();
+		getServiceById();
+	}, []);
 
-  const getStudio = async () => {
-    await axios
-      .get(
-        import.meta.env.VITE_REACT_APP_API_URL +
-          `/Studio/GetStudioByID/${studioId}`
-      )
-      .then((res) => {
-        // console.log(res.data);
-        setStudioLogo(res.data.logo);
-        setStudioName(res.data.studioName);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+	const getStudio = async () => {
+		await axios
+			.get(
+				import.meta.env.VITE_REACT_APP_API_URL +
+					`/Studio/GetStudioByID/${studioId}`
+			)
+			.then((res) => {
+				// console.log(res.data);
+				setStudioLogo(res.data.logo);
+				setStudioName(res.data.studioName);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-  const getServiceById = async () => {
-    await axios
-      .get(
-        import.meta.env.VITE_REACT_APP_API_URL +
-          `/Service/v2/GetServiceByID/${serviceId}`
-      )
-      .then((res) => {
-        // console.log(res.data);
-        setData(res.data); //data: {$id, service, studio}
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+	const getServiceById = async () => {
+		await axios
+			.get(
+				import.meta.env.VITE_REACT_APP_API_URL +
+					`/Service/v2/GetServiceByID/${serviceId}`
+			)
+			.then((res) => {
+				// console.log(res.data);
+				setData(res.data); //data: {$id, service, studio}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-  const handleOnClickService = (event) => {
-    navigate(`/services/${serviceId}`);
-  };
+	const handleOnClickService = (event) => {
+		navigate(`/services/${serviceId}`);
+	};
 
-  const handleOnClickBooking = (event) => {
-    setOpen(true);
-    console.log(2);
-  };
+	const handleOnClickBooking = (event) => {
+		setOpen(true);
+		console.log(2);
+	};
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+	const handleClose = () => {
+		setOpen(false);
+	};
 
-  return (
-    <Card sx={{ width: "90%" }} key={serviceId}>
-      <CardMedia
-        onClick={handleOnClickService}
-        component="img"
-        alt={serviceName}
-        image={imageService}
-        sx={{
-          minHeight: 200,
-          maxHeight: 200,
-          cursor: "pointer",
-        }}
-      />
-      <CardContent>
-        <Stack
-          spacing={3}
-          direction={"row"}
-          justifyContent={"flex-start"}
-          alignItems={"center"}
-        >
-          <Avatar alt={studioName} src={studioLogo} />
-          <Typography>{studioName}</Typography>
-        </Stack>
-      </CardContent>
-      <CardContent>
-        <Stack spacing={3} sx={{ position: "relative" }}>
-          <Stack spacing={1}>
-            <Typography
-              className="ellipsis-serviceName"
-              variant="body2"
-              color="#7C7676"
-            >
-              {serviceName}
-            </Typography>
-            <Rating size="small" value={rate} readOnly />
-            <Typography variant="subtitle2">Cost: {price} VNĐ</Typography>
-          </Stack>
-          <CardActions>
-            <Button
-              size="medium"
-              variant="outlined"
-              sx={{ position: "absolute", right: 0 }}
-              onClick={handleOnClickBooking}
-            >
-              <Typography variant="body2" color="#FF7F22">
-                Booking
-              </Typography>
-            </Button>
-            <Modal
-              open={open}
-              onClose={handleClose}
-              aria-labelledby="modal-modal-title"
-              aria-describedby="modal-modal-description"
-            >
-              <Booking data={data} />
-            </Modal>
-          </CardActions>
-        </Stack>
-      </CardContent>
-    </Card>
-  );
+	return (
+		<Card sx={{ width: "90%" }} key={serviceId}>
+			<CardMedia
+				onClick={handleOnClickService}
+				component="img"
+				alt={serviceName}
+				image={imageService}
+				sx={{
+					minHeight: 200,
+					maxHeight: 200,
+					cursor: "pointer",
+				}}
+			/>
+			<CardContent>
+				<Stack
+					spacing={3}
+					direction={"row"}
+					justifyContent={"flex-start"}
+					alignItems={"center"}
+				>
+					<Avatar alt={studioName} src={studioLogo} />
+					<Typography>{studioName}</Typography>
+				</Stack>
+			</CardContent>
+			<CardContent>
+				<Stack spacing={3} sx={{ position: "relative" }}>
+					<Stack spacing={1}>
+						<Typography
+							className="ellipsis-serviceName"
+							variant="body2"
+							color="#7C7676"
+						>
+							{serviceName}
+						</Typography>
+						<Rating size="small" value={rate} readOnly />
+						<Typography variant="subtitle2">
+							Cost: {price} VNĐ
+						</Typography>
+					</Stack>
+					<CardActions>
+						<Button
+							size="medium"
+							variant="outlined"
+							sx={{ position: "absolute", right: 0 }}
+							onClick={handleOnClickBooking}
+						>
+							<Typography variant="body2" color="#FF7F22">
+								Booking
+							</Typography>
+						</Button>
+						<Modal
+							open={open}
+							onClose={handleClose}
+							aria-labelledby="modal-modal-title"
+							aria-describedby="modal-modal-description"
+						>
+							<Booking data={data} />
+						</Modal>
+					</CardActions>
+				</Stack>
+			</CardContent>
+		</Card>
+	);
 };
 
 export default CardService;
--- src/components/Modal/FormBooking.jsx
+++ src/components/Modal/FormBooking.jsx
@@ -1,54 +1,54 @@
 import {
-  Box,
-  Button,
-  Container,
-  Stack,
-  TextField,
-  Typography,
+	Box,
+	Button,
+	Container,
+	Stack,
+	TextField,
+	Typography,
 } from "@mui/material";
 import { DatePicker, DateTimePicker, TimePicker } from "@mui/x-date-pickers";
 import React, { useState } from "react";
 import SliderSlotTime from "../Slider/SliderSlotTime";
 
 const FormBooking = ({
-  nameRef,
-  phoneRef,
-  dateTimeRef,
-  dateRef,
-  timeRef,
-  studioId,
+	nameRef,
+	phoneRef,
+	dateTimeRef,
+	dateRef,
+	timeRef,
+	studioId,
 }) => {
-  return (
-    <Container maxWidth="sm">
-      <Stack spacing={2} paddingTop={3} textAlign={"center"}>
-        <Typography variant="h5">Contact Info</Typography>
-        <Box>
-          <Stack spacing={4}>
-            <TextField
-              id="user"
-              label="Username"
-              variant="outlined"
-              inputRef={nameRef}
-              required
-            />
-            <TextField
-              id="phone"
-              label="PhoneNumber"
-              variant="outlined"
-              inputRef={phoneRef}
-              type="tel"
-              pattern="[0-9]{10}"
-              required
-            />
-            {/* <DateTimePicker inputRef={dateTimeRef} required /> */}
-            <DatePicker inputRef={dateRef} required />
-            {/* <TimePicker inputRef={timeRef} required /> */}
-            <SliderSlotTime studioId={studioId} timeRef={timeRef} />
-          </Stack>
-        </Box>
-      </Stack>
-    </Container>
-  );
+	return (
+		<Container maxWidth="sm">
+			<Stack spacing={2} paddingTop={3} textAlign={"center"}>
+				<Typography variant="h5">Contact Info</Typography>
+				<Box>
+					<Stack spacing={4}>
+						<TextField
+							id="user"
+							label="Username"
+							variant="outlined"
+							inputRef={nameRef}
+							required
+						/>
+						<TextField
+							id="phone"
+							label="PhoneNumber"
+							variant="outlined"
+							inputRef={phoneRef}
+							type="tel"
+							pattern="[0-9]{10}"
+							required
+						/>
+						{/* <DateTimePicker inputRef={dateTimeRef} required /> */}
+						<DatePicker inputRef={dateRef} required />
+						{/* <TimePicker inputRef={timeRef} required /> */}
+						<SliderSlotTime studioId={studioId} timeRef={timeRef} />
+					</Stack>
+				</Box>
+			</Stack>
+		</Container>
+	);
 };
 
 export default FormBooking;
